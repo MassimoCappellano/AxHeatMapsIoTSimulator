13d7748d02696bfaf2492bbc28b1e0686f9287d5
Massimo Cappellano <Massimo.Cappe@gmail.com>
2017-11-23 19:12:06 +0100

    improvement

--- createTopology.js
+++ createTopology.js
@@ -1,14 +1,26 @@
 'use strict'
-var fs = require('fs');
-var f1 = require('./topologyLights');
 
-var strTopology = f1(100);
+const fs = require('fs');
+const env = require('env2')('./.env');
+const f1 = require('./topologyLights');
 
-console.log(strTopology);
+console.log("NUM_LIGHTS:", process.env.NUM_LIGHTS); 
+console.log("PATH_OUTPUT_GEOMAP:", process.env.PATH_OUTPUT_GEOMAP); 
 
-var stream = fs.createWriteStream("./out/Geomap.txt");
+var pathOutputGeomap = process.env.PATH_OUTPUT_GEOMAP || "./out/Geomap_out.txt";
+var numLights = process.env.NUM_LIGHTS || 100;
+
+if(isNaN(numLights = parseInt(numLights, 10))) {
+  numLights = 100;
+} 
+
+var strTopology = f1(numLights);
+
+var stream = fs.createWriteStream(pathOutputGeomap);
 
 stream.once('open', function(fd) {
   stream.write(strTopology);
   stream.end();
 });
+
+console.log("FILE TOPOLOGY CREATED!!!");
