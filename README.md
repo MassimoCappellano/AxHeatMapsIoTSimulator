dedeaa64fb1942d158e6a9c27d2462fd6f002863
Massimo Cappellano <Massimo.Cappe@gmail.com>
2017-11-25 10:08:10 +0100

    improvements

--- README.md
+++ README.md
@@ -2,7 +2,9 @@
 
 `node ./createTopology.js`
 
-creazione file Geomap
+programma per creazione di file formato Geomap.
+
+Esempio formato file Geomap:
 
 `ID000;GW HeatMap;00158D00019A5E07;0.0034936043;0.0045115078;0|ID001;Lamp 1;00158D00010A4C57;0.0032502170;0.0043601978;1|ID002;Lamp 2;00158D00010A4C47;0.0034158439;0.0043624436;1|ID003;Lamp 3;00158D00010A48EC;0.0035820322;0.0043607593;1|ID004;Lamp 4;00158D00010A4C67;0.0037889254;0.0043610400;1|`
 
@@ -11,6 +13,11 @@ creazione file Geomap
 Creare un file `.env` basandosi sul file `.env_template`
 
 `NUM_LIGHTS` e `PATH_OUTPUT_GEOMAP`
+
+| **PARAMETRO**      | **DESCRIZIONE**           | **DEFAULT**  |
+| ------------- |-------------| -----|
+| `NUM_LIGHTS`      | numero di luci per cui viene generato il file Geomap. Valore massimo 150, | `100` |
+| `PATH_OUTPUT_GEOMAP`      | path di output file Geomap      |   `./out/Geomap_out.txt` |
 
 ## TRAFFIC GENERATOR
 
--- createTopology.js
+++ createTopology.js
@@ -2,19 +2,30 @@
 
 const fs = require('fs');
 const env = require('env2')('./.env');
-const f1 = require('./topologyLights');
+const topologyLights = require('./topologyLights');
+
+const DEFAULT_NUM_LIGHTS = 100;
+const DEFAULT_PATH_OUTPUT_GEOMAP = "./out/Geomap_out.txt";
 
 console.log("NUM_LIGHTS:", process.env.NUM_LIGHTS); 
 console.log("PATH_OUTPUT_GEOMAP:", process.env.PATH_OUTPUT_GEOMAP); 
 
-var pathOutputGeomap = process.env.PATH_OUTPUT_GEOMAP || "./out/Geomap_out.txt";
-var numLights = process.env.NUM_LIGHTS || 100;
+var pathOutputGeomap = process.env.PATH_OUTPUT_GEOMAP || DEFAULT_PATH_OUTPUT_GEOMAP;
+var numLights = process.env.NUM_LIGHTS || DEFAULT_NUM_LIGHTS;
 
-if(isNaN(numLights = parseInt(numLights, 10))) {
-  numLights = 100;
+if((typeof numLights) === 'string' && isNaN(numLights = parseInt(numLights, 10))) {
+  console.log("VALUE NUM_LIGHTS non numero,  settato a default:",
+    DEFAULT_NUM_LIGHTS);
+  numLights = DEFAULT_NUM_LIGHTS;
 } 
 
-var strTopology = f1(numLights);
+if(numLights > 150) {
+  console.log("NUM_LIGHTS >", 150, 
+    "settato a default:", DEFAULT_NUM_LIGHTS);
+  numLights = DEFAULT_NUM_LIGHTS;
+}
+
+var strTopology = topologyLights(numLights);
 
 var stream = fs.createWriteStream(pathOutputGeomap);
 
